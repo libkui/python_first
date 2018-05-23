9d88844c0ee5eee97f794b4300421e43925bd57b
cqbomb <605658506@qq.com>
2018-05-23 09:18:18 +0800

    img

--- Charpter4_Python内置类型_字符串与正则表达式/re_mac.py
+++ Charpter4_Python内置类型_字符串与正则表达式/re_mac.py
@@ -13,6 +13,7 @@ str1 = '166    54a2.74f7.0326    DYNAMIC     Gi1/0/11'
 result = re.match('(\d{1,4})\s+([0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4})\s+(\w+)\s+(\w.*\d)',str1).groups()
 
 print('-'*80)
-print('%-10s : %s' % ('VLAN ID', result[1]))
-print('%-10s : %s' % ('MAC', result[2]))
+print('%-10s : %s' % ('VLAN ID', result[0]))
+print('%-10s : %s' % ('MAC', result[1]))
+print('%-10s : %s' % ('Type', result[2]))
 print('%-10s : %s' % ('Interface', result[3]))
