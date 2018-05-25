764639d36a986b95090ad068ba6452e3e8491e4d
cqbomb <605658506@qq.com>
2018-05-25 17:45:36 +0800

    修改第十四章作业图

--- Charpter13_Python类/person_1_start.py
+++ Charpter13_Python类/person_1_start.py
@@ -7,11 +7,12 @@
 # https://ke.qq.com/course/271956?tuin=24199d8a
 
 class Person:
-    def __init__(self, name, age, pay = 0, job = None):
+    def __init__(self, name, age, pay=0, job=None):
         self.name = name
         self.age = age
         self.pay = pay
         self.job = job
+
 
 if __name__ == '__main__':
     bob = Person('Bob Smith', 42, 30000, 'software')
