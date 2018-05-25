f100b13d41f6c5f68ccca3d9b74be09cba1d8641
cqbomb <605658506@qq.com>
2018-05-25 10:06:12 +0800

    修改第十三章作业图

--- Charpter13_Python类/class_8_final.py
+++ Charpter13_Python类/class_8_final.py
@@ -6,6 +6,7 @@
 # 教主技术进化论拓展你的技术新边疆
 # https://ke.qq.com/course/271956?tuin=24199d8a
 
+
 class Person:
     """
     一般Person：数据+逻辑
@@ -15,12 +16,16 @@ class Person:
         self.age = age
         self.pay = pay
         self.job = job
+
     def lastName(self):
         return self.name.split()[-1]
+
     def giveRaise(self,percent):
         self.pay *= (1.0 + percent)
+
     def __str__(self):
         return '<%s => %s: %s, %s >' % (self.__class__.__name__, self.name, self.job, self.pay)
+
 
 class Manager(Person):
     """
@@ -29,8 +34,10 @@ class Manager(Person):
     """
     def __init__(self, name, age, pay):
         Person.__init__(self, name, age, pay, 'manager')
+
     def giveRaise(self, percent, bonus = 0.1):
         Person.giveRaise(self, percent + bonus)
+
 
 if __name__ == '__main__':
     bob = Person('Bob Smith', 42, 30000, 'software')
--- Charpter13_Python类/manager_7_restructure.py
+++ Charpter13_Python类/manager_7_restructure.py
@@ -8,11 +8,14 @@
 
 from person_5_format_str import Person
 
+
 class Manager(Person):
     def __init__(self, name, age, pay):
         Person.__init__(self, name, age, pay, 'manager')
+
     def giveRaise(self, percent, bonus = 0.1):
         Person.giveRaise(self, percent + bonus)
+
 
 if __name__ == '__main__':
     tom = Manager(name = 'Tom Doe', age = 50, pay = 50000)
