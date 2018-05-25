ae90141403f9cbb118c669276890e5314ef5f520
cqbomb <605658506@qq.com>
2018-05-25 17:02:18 +0800

    修改第十四章作业图

--- Charpter14_內建模块os,sys,datetime,optparse/arg_parse.py
+++ Charpter14_內建模块os,sys,datetime,optparse/arg_parse.py
@@ -6,7 +6,8 @@
 # 教主技术进化论拓展你的技术新边疆
 # https://ke.qq.com/course/271956?tuin=24199d8a
 
-def qyt_argparse(filename, iface):
+def qyt_argparse(host, filename, iface):
+    print(host)
     print(filename)
     print(iface)
 
@@ -14,16 +15,15 @@ def qyt_argparse(filename, iface):
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
-    usage = "usage: python testargparse.py -f filename -i interface"
+    usage = "usage: python arg_parse.py ipaddress -f filename -i interface"
 
     parser = ArgumentParser(usage=usage)
 
-    parser.add_argument("-f", "--file", dest="filename", help="Write report to FILE", default='defaultFilename', type= str)
-    parser.add_argument("-i", "--interface", dest="iface", help="Specify an interface", default=1, type= int)
+    parser.add_argument("-f", "--file", dest="filename", help="Write content to FILE", default='1.txt', type=str)
+    parser.add_argument("-i", "--interface", dest="iface", help="Specify an interface", default=1, type=int)
     parser.add_argument(nargs='?', dest="host", help="Specify an host", default='10.1.1.1', type=str)
-    #parser.add_argument(nargs='*', dest="hosts", help="Specify an host", default='10.1.1.1', type=str)
+    # parser.add_argument(nargs='*', dest="hosts", help="Specify an host", default='10.1.1.1', type=str)
     args = parser.parse_args()
 
-    print(args.host)
-    #print(args.hosts)
-    qyt_argparse(args.filename, args.iface)
+    qyt_argparse(args.host, args.filename, args.iface)
+    # qyt_argparse(args.hosts, args.filename, args.iface)
