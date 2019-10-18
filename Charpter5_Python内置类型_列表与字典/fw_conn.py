6a583952e4331be45c71ee3df7faab01398d8b61
collinsctk@qytang.com <collinsctk@qytang.com>
2019-10-18 18:19:48 +0800

    2019-10-18

--- Charpter5_Python内置类型_列表与字典/fw_conn.py
+++ Charpter5_Python内置类型_列表与字典/fw_conn.py
@@ -8,6 +8,7 @@
 
 import re
 
+
 str1 = """TCP Student  192.168.189.167:32806 Teacher  137.78.5.128:65247, idle 0:00:00, bytes 74, flags UIO 
 TCP Student  192.168.189.167:80 Teacher  137.78.5.128:65233, idle 0:00:03, bytes 334516, flags UIO """
 
@@ -15,9 +16,7 @@ list1 = str1.split('\n')
 
 dict1 = {}
 for x in list1:
-    result = re.match(
-        ".*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5}).*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5}).*bytes\s+("
-        "\d+).*flags\s+(\w*)\s*", x).groups()
+    result = re.match(".*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5}).*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5}).*bytes\s+(\d+).*flags\s+(\w*)\s*", x).groups()
     qyt_key = result[0], result[1], result[2], result[3]
     qyt_value = result[4], result[5]
     dict1[qyt_key] = qyt_value
