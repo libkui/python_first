555d4aa9c7bc472174a605a8227f335357b4b0cd
collinsctk@qytang.com <collinsctk@qytang.com>
2019-10-18 17:54:52 +0800

    2019-10-18

--- Charpter3_Python内置类型_数字/random_ip.py
+++ Charpter3_Python内置类型_数字/random_ip.py
@@ -6,15 +6,14 @@
 # 教主技术进化论拓展你的技术新边疆
 # https://ke.qq.com/course/271956?tuin=24199d8a
 
-import random#导入random模块
 
-#随机产生IP地址四个段落的数字
-section1 = <学员填写>
-section2 = <学员填写>
-section3 = <学员填写>
-section4 = <学员填写>
+# 随机产生IP地址四个段落的数字
+section1 = "<学员填写>"
+section2 = "<学员填写>"
+section3 = "<学员填写>"
+section4 = "<学员填写>"
 
 random_ip = str(section1) + '.' + str(section2) + '.' + str(section3) + '.' + str(section4)
-#要把数字转换成为字符串，并且拼接在一起！大家可以想象不转换的结果是什么？
+# 要把数字转换成为字符串，并且拼接在一起！大家可以想象不转换的结果是什么？
 print(random_ip)
-#打印结果
+# 打印结果
