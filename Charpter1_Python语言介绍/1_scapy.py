0b7fb4c6abbb916961c3a0ca9ff3a96c240ac991
collinsctk@qytang.com <collinsctk@qytang.com>
2019-10-18 17:54:09 +0800

    2019-10.18

--- Charpter1_Python语言介绍/1_scapy.py
+++ Charpter1_Python语言介绍/1_scapy.py
@@ -1,6 +1,6 @@
-from scapy.all import *
 import logging
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)  # 清除报错
+logging.getLogger("kamene.runtime").setLevel(logging.ERROR)
+from kamene.all import *
 
-ping_one_reply = sr1(IP(dst='137.78.5.254')/ICMP(), timeout=1, verbose=False)
+ping_one_reply = sr1(IP(dst='196.21.5.254')/ICMP(), timeout=1, verbose=False)
 ping_one_reply.show()
